4677f7619c6e229f3c3f4392982fc08974cd9484
lufeifei <1251200096@qq.com>
2021-01-11 15:15:19 +0800

    1、飞机项目

---
+++ src/Customer.java
@@ -0,0 +1,38 @@
+public class Customer {
+    private  String id;
+    private  String name;
+    private  String phone;
+    private  String carId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+}
